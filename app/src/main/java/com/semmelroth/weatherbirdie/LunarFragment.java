4b0e3045b04698faed4162e96181bfa8e0707fd2
Nikki Semmelroth <nikkisemmelroth@gmail.com>
2018-11-26 21:53:55 -0700

    Adding more fragments that will eventually be tabs

--- app/src/main/java/com/semmelroth/weatherbirdie/LunarFragment.java
+++ app/src/main/java/com/semmelroth/weatherbirdie/LunarFragment.java
@@ -1,4 +1,5 @@
-package com;
+package com.semmelroth.weatherbirdie;
+
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -6,23 +7,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.semmelroth.weatherbirdie.R;
+public class LunarFragment extends Fragment {
 
-public class SummaryFragment extends Fragment {
-
-    public SummaryFragment() {
+    public LunarFragment() {
         // Required empty public constructor
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    public void onCreate(Bundle savedInstanceState) { super.onCreate(savedInstanceState);}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_summary, container, false);
+        return inflater.inflate(R.layout.fragment_lunar, container, false);
     }
+
 }
